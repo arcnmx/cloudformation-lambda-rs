66264461a9e4806dd879d30dd58c6d10443630aa
arcnmx <arcnmx@users.noreply.github.com>
2017-07-31 23:45:37 -0400

    Content-Length header in responses

--- src/response.rs
+++ src/response.rs
@@ -1,9 +1,8 @@
-use std::io;
-use hyper::{status, method};
+use std::io::{self, Write};
+use hyper::{status, method, header};
 use hyper::client::Request;
 use hyper::net::HttpsConnector;
 use hyper_native_tls::NativeTlsClient;
-use serde::Serialize;
 use serde_json;
 use url::Url;
 
@@ -11,14 +10,27 @@ use model::CloudFormationResponse;
 use Error;
 
 pub fn send(url: Url, response: CloudFormationResponse) -> Result<(), Error> {
+    // Need to know Content-Length beforehand...
+    let response_json = serde_json::to_string(&response)?;
+
     let tls = NativeTlsClient::new()?;
-    let mut request = Request::with_connector(method::Method::Put, url, &HttpsConnector::new(tls))?.start()?;
-    response.serialize(&mut serde_json::Serializer::new(&mut request))?;
+    let mut request = Request::with_connector(method::Method::Put, url, &HttpsConnector::new(tls))?;
+    request.headers_mut().set(header::ContentLength(response_json.len() as _));
+    let mut request = request.start()?;
+    io::copy(&mut response_json.as_bytes(), &mut request)?;
     let mut response = request.send()?;
+
     println!("Got response: {:#?}", response);
 
     if response.status != status::StatusCode::Ok {
-        let _ = io::copy(&mut response, &mut io::stderr());
+        // Print out response body
+        let stderr = io::stderr();
+        {
+            let mut stderr = stderr.lock();
+            let _ = io::copy(&mut response, &mut stderr);
+            let _ = stderr.flush();
+        }
+
         Err(Box::new(io::Error::new(io::ErrorKind::Other, format!("CloudFormation response failed with {}", response.status))))
     } else {
         Ok(())
